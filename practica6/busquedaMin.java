4d03a594d4da93a831b2a81713d55b2142466e37
AroldoMunoz <83074122+AroldoMunoz@users.noreply.github.com>
2021-11-22 00:54:31 -0500

    tercer ejercicio
    buscar el numero menor de un array rotado

---
+++ practica6/busquedaMin.java
@@ -0,0 +1,46 @@
+package practica6;
+//encontrar menor elemento de un array ordenado rotado 
+//complejidad O(log_2(n))  pues cada ves q entra al while hace una particion del arreglo en 2
+public class busquedaMin {
+	public static int minimo( int [] arreglo) {
+		 int inicio = 0;
+		 int fin = arreglo.length - 1;
+		 int pos,dato;
+		 while (inicio <= fin) {
+			 pos = (inicio+fin) / 2;
+		     dato=arreglo[pos];
+		     if ( arreglo[inicio] < dato &&dato<arreglo[fin] )
+		       return arreglo[fin];
+		     else if ( arreglo[inicio] >= dato ) {
+		    	 fin = pos;
+		    	  	} else {
+		     	inicio = pos;
+		     	  	}
+		        
+		     if ( inicio==fin) {
+				 return arreglo[inicio+1];
+			 }
+		 }
+		 return -1;
+		    }
+	
+	public static void main(String []args){
+		//caso prueba
+		int[] arra= {6,7,9,15,19,2,3};
+		int a=minimo(arra);
+		if (a!=-1)
+		System.out.println("el numero menor es: "+a);
+		else
+			System.out.println("-1");
+		//caso 2
+		int[] array= {7,8,9,10,1,2,3,4,5,6};
+		
+		int n=minimo(array);
+		if (n!=-1)
+		System.out.println("el numero menor es: "+n);
+		else
+			System.out.println("-1");
+		
+	}
+
+}
